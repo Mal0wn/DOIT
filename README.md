f9f9a458dbe5f2019bbf8494a51e5aab4fd6740a
implementation pod for new connector <marine.michelot-cic@ibm.com>
2023-02-06 13:41:22 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,50 @@
+# Do It 4 Me - Mobile
+Node v 17.4.0
+React Native 0.7
+React 18.1
+TS 4.9
+
+## Installation
+Clone the project 
+
+```
+npm i 
+```
+
+## Run application 
+### Run instructions for :robot: **Android** :robot: : 
+:round_pushpin: Have an Android emulator running (quickest way to get started), or a device connected.
+
+:round_pushpin: And run ```npx react-native run-android``` 
+
+### Run instructions for :iphone: **iOS** :iphone: : 
+:round_pushpin: Run ```npx react-native run-ios``` and reload the app after build (touch reload in the simulator or "r" in terminal)
+:round_pushpin: Or Open doIt4meReactNative.xcworkspace in Xcode 
+:round_pushpin: Or run ```xed -b ios``` 
+:round_pushpin: Hit the Run Button
+
+### Run instructions for :desktop_computer: macOS :desktop_computer: : 
+:round_pushpin: See [React Native Guide for MacOS](https://aka.ms/ReactNativeGuideMacOS) for the latest up-to-date instructions.
+
+### See your changes 
+Press **Cmd + R** in the simulator to reload your app's code 
+
+### Debug 
+Press **Cmd + D** in the simulator or Shake your device to open the React Native debug menu 
+
+#### Learn More 
+
+:baby: [The Basics](https://reactnative.dev/docs/tutorial)
+
+:crown: [Style](https://reactnative.dev/docs/style)
+
+:package: [Layout with Flexbox](https://reactnative.dev/docs/flexbox)
+
+:computer:[Core Components and APIs](https://reactnative.dev/docs/components-and-apis)
+
+:boat: [Navigation](https://reactnative.dev/docs/navigation)
+
+:satellite: [Networking](https://reactnative.dev/docs/network)
+
+
+
---
+++ src/App.tsx
@@ -0,0 +1,74 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+
+import {
+  SafeAreaView,
+  ScrollView,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+
+import {Colors, Header} from 'react-native/Libraries/NewAppScreen';
+
+function HomeScreen() {
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>Home Screen</Text>
+    </View>
+  );
+}
+
+const Stack = createNativeStackNavigator();
+
+function App(): JSX.Element {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  return (
+    <SafeAreaView style={styles.main}>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Home" component={HomeScreen} />
+          <Stack.Screen name="Home2" component={HomeScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  main: {
+    flex: 1,
+  },
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+});
+
+export default App;
