c86180be449919fa4259a46226e1e9110ed5ad2e
implementation pod for new connector <marine.michelot-cic@ibm.com>
2023-02-08 10:29:12 +0100

    clean code

--- src/DisplayMission.js
+++ src/DisplayMission.js
@@ -21,18 +21,14 @@ function DisplayMission({ route, navigation }) {
 
   const [data, setData] = useState([{ title: "", description: "", price: 0, id_create: 0 }]);
   const [dataUser, setUser] = useState([{ firstname: "", lastname:"" , id:0 }]);
-  const [loading, setLoading] = useState(false)
 
   const { id } = route.params
  
-
-
   const fetchMission = async (idMission) => {
     await axios
     .get("http://localhost:3000/mission/" + idMission)
     .then((res) => {
-      setData(res.data)
-      console.log(res.data)
+      setData(res.data[0])
       fetchUser(res.data[0].id_create)
     })
     .catch((err) => console.log(err));
@@ -42,31 +38,26 @@ function DisplayMission({ route, navigation }) {
     await axios
     .get("http://localhost:3000/user/" + idCreate)
     .then((res) => {
-      setUser(res.data)
-      console.log(res.data)
-      
+      setUser(res.data[0])
     })
     .catch((err) => console.log(err));
   }
 
   useEffect(() => {
     fetchMission(id)
-   
-    //fetchUser()
   }, [])
 
   return (
     <View style={{ flex: 1, padding: 24 }}>
-
       <View style={styles.box}>
         <View style={styles.containTitle}>
-          <Text style={styles.title}> {data[0].title}</Text>
+          <Text style={styles.title}> {data.title}</Text>
         </View>
         <View style={styles.containDescription}>
-          <Text style={styles.description}>{data[0].description}</Text>
+          <Text style={styles.description}>{data.description}</Text>
           <View>
-            <Text>Prix :  {data[0].price} € </Text>
-            <Text>Proposée par : {dataUser[0].firstname}  </Text>
+            <Text>Prix :  {data.price} € </Text>
+            <Text>Proposée par : {dataUser.firstname} {dataUser.lastname}  </Text>
           </View>
         </View>
         
