7f260949d1b5f2ab8c8766fdee4122daad0cd5e7
implementation pod for new connector <marine.michelot-cic@ibm.com>
2023-02-08 10:43:06 +0100

    clean

--- src/DisplayMission.js
+++ src/DisplayMission.js
@@ -2,45 +2,36 @@ import React, { useEffect, useState } from 'react';
 import { useNavigation } from '@react-navigation/native';
 import axios from "axios";
 
-
 import {
-  SafeAreaView,
-  ScrollView,
   StyleSheet,
   Text,
-  FlatList,
-  useColorScheme,
   View,
-  TouchableOpacity,
-  Button,
   Pressable
 } from 'react-native';
-
 
 function DisplayMission({ route, navigation }) {
 
   const [data, setData] = useState([{ title: "", description: "", price: 0, id_create: 0 }]);
-  const [dataUser, setUser] = useState([{ firstname: "", lastname:"" , id:0 }]);
-
+  const [dataUser, setUser] = useState([{ firstname: "", lastname: "", id: 0 }]);
   const { id } = route.params
- 
+
   const fetchMission = async (idMission) => {
     await axios
-    .get("http://localhost:3000/mission/" + idMission)
-    .then((res) => {
-      setData(res.data[0])
-      fetchUser(res.data[0].id_create)
-    })
-    .catch((err) => console.log(err));
+      .get("http://localhost:3000/mission/" + idMission)
+      .then((res) => {
+        setData(res.data[0])
+        fetchUser(res.data[0].id_create)
+      })
+      .catch((err) => console.log(err));
   }
 
   const fetchUser = async (idCreate) => {
     await axios
-    .get("http://localhost:3000/user/" + idCreate)
-    .then((res) => {
-      setUser(res.data[0])
-    })
-    .catch((err) => console.log(err));
+      .get("http://localhost:3000/user/" + idCreate)
+      .then((res) => {
+        setUser(res.data[0])
+      })
+      .catch((err) => console.log(err));
   }
 
   useEffect(() => {
@@ -60,17 +51,16 @@ function DisplayMission({ route, navigation }) {
             <Text>Proposée par : {dataUser.firstname} {dataUser.lastname}  </Text>
           </View>
         </View>
-        
-    <Pressable 
-    style={styles.button} 
-    onPress={() => console.log('Contacter')}>
-    <Text style={styles.textButton}>Contacter</Text>
-    </Pressable>
+        <Pressable
+          style={styles.button}
+          onPress={() => console.log('Contacter')}>
+          <Text style={styles.textButton}>Contacter</Text>
+        </Pressable>
       </View>
     </View>
 
   );
-  
+
 }
 
 export default DisplayMission;
@@ -79,45 +69,45 @@ export default DisplayMission;
 const styles = StyleSheet.create({
 
   box: {
-    backgroundColor:"#5C9EAD",
-    borderRadius : 5 ,
-    display : "flex",
-    justifyContent : "center",
-    textAlign : "center"
+    backgroundColor: "#5C9EAD",
+    borderRadius: 5,
+    display: "flex",
+    justifyContent: "center",
+    textAlign: "center"
 
   },
   containTitle: {
     backgroundColor: "#7D1D3F",
     borderTopLeftRadius: 5,
     borderTopRightRadius: 5,
-    padding : 10
+    padding: 10
   },
   title: {
-    color : "#FFFFFF",
-    fontWeight : "bold"
+    color: "#FFFFFF",
+    fontWeight: "bold"
   },
   containDescription: {
-    
-    padding : 10
+
+    padding: 10
   },
   description: {
     textAlign: "justify"
   },
   button: {
     backgroundColor: "#7D1D3F",
-    padding : 5,
-    borderRadius : 5 ,
-    display : "flex",
-    justifyContent : "center",
-    textAlign : "center",
-    marginTop : 20,
+    padding: 5,
+    borderRadius: 5,
+    display: "flex",
+    justifyContent: "center",
+    textAlign: "center",
+    marginTop: 20,
     width: "50%",
     marginLeft: "auto",
     marginRight: "auto",
     marginBottom: 20
   },
   textButton: {
-    textAlign : "center",
+    textAlign: "center",
     color: "#FFFFFF",
   }
 })
